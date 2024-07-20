a718fd595125a869a39ecd3e7822335bb29a5e58
Anders Pitman <&ers@apitman.com>
2024-07-20 13:43:59 -0600

    Implement updating colors

--- palette.js
+++ palette.js
@@ -19,15 +19,13 @@ const template = `
 
   </style>
 
-  <div class='color-list'>
-    <div class='color' style='background-color: red;'></div>
-    <div class='color' style='background-color: blue;'></div>
-    <div class='color' style='background-color: white;'></div>
+  <div class='colors'>
   </div>
-  <div class='color-list'>
-    <div class='color' style='background-color: purple;'></div>
-    <div class='color' style='background-color: orange;'></div>
-  </div>
+  <div class='color-pickers'>
+    <input class='color-picker' type='color' />
+    <input class='color-picker' type='color' />
+    <input class='color-picker' type='color' />
+  <div>
 `;
 
 let templateEl;
@@ -48,23 +46,86 @@ class Palette extends HTMLElement {
 
     const docFrag = templateEl.content.cloneNode(true);
 
-    const colorListEls = docFrag.querySelectorAll('.color-list');
+    const allColors = [
+      ['#000000', '#ffffff'],
+      ['#ff0000', '#00ff00', '#0000ff'],
+    ];
 
-    for (const colorListEl of colorListEls) {
+    let selectedIdx = 0;
 
-      const colorEls = colorListEl.querySelectorAll('.color');
-      const colors = Array.from(colorEls).map((ce) => ce.style['background-color']);
+    const colorsEl = docFrag.querySelector('.colors');
+
+    for (let i=0; i<allColors.length; i++) {
+
+      const colors = allColors[i];
+
+      const colorListEl = document.createElement('div');
+      colorListEl.classList.add('color-list');
+      colorsEl.appendChild(colorListEl);
 
       colorListEl.addEventListener('click', (evt) => {
-        this.shadowRoot.dispatchEvent(new CustomEvent('colors-selected', {
+
+        selectedIdx = i;
+
+        updatePickers();
+        updateColors();
+      });
+
+      for (const color of colors) {
+        const colorEl = document.createElement('div');
+        colorEl.classList.add('color');
+        colorListEl.appendChild(colorEl);
+        colorEl.style['background-color'] = color;
+      }
+    }
+
+    const pickersContainerEl = docFrag.querySelector('.color-pickers');
+    const pickerEls = pickersContainerEl.querySelectorAll('.color-picker');
+
+    for (let i=0; i<pickerEls.length; i++) {
+      const pickerEl = pickerEls[i];
+
+      pickerEl.addEventListener('change', (evt) => {
+        const color = evt.target.value;
+        allColors[selectedIdx][i] = color;
+        updateColors();
+      });
+    }
+
+    const updatePickers = () => {
+
+      for (let i=0; i<pickerEls.length; i++) {
+        const pickerEl = pickerEls[i];
+
+        pickerEl.value = allColors[selectedIdx][i];
+      }
+    };
+
+    const updateColors = () => {
+      const colorListEls = colorsEl.querySelectorAll('.color-list');
+      for (let i=0; i<colorListEls.length; i++) {
+
+        const colorListEl = colorListEls[i];
+
+        const colorEls = colorListEl.querySelectorAll('.color');
+
+        for (let j=0; j<colorEls.length; j++) {
+          colorEls[j].style['background-color'] = allColors[i][j];
+        }
+
+      }
+
+      this.shadowRoot.dispatchEvent(new CustomEvent('colors-selected', {
           bubbles: true,
           composed: true,
           detail: {
-            colors,
+            colors: allColors[selectedIdx],
           },
         }));
-      });
-    }
+    };
+
+    updatePickers();
+    updateColors();
 
     this.shadowRoot.appendChild(docFrag);
   }
